6bb201544b0560dd6bfe49904727d471961a24c8
Shail <shailjain2709@gmail.com>
2024-10-01 11:28:50 +0530

    Implemented user signup, login, and logout functionality

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -47,11 +47,44 @@ export const signup = async (req, res) => {
 };
 
 //Logging in the User
-export const login = (req, res) => {
-  res.send("Login routes");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }).select("+password");
+    if (!user) {
+      return res.status(400).json({ error: "Invalid Username or Password" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid Username or Password" });
+    }
+    sendCookie(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 //Logging Out the User
 export const logout = (req, res) => {
-  res.send("Logout routes");
+  try {
+    res
+      .status(200)
+      .cookie("jwt", "", {
+        maxAge: 0,
+      })
+      .json({
+        success: true,
+        message: "Successfully logged out",
+      });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
