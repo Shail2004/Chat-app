d831be28ad063d92d7eabb7de1f9233f5b4caf9d
Shail <shailjain2709@gmail.com>
2024-10-02 11:39:07 +0530

    Implemented user fetching functionality for chat sidebar

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import { connectDB } from "./data/database.js";
 import cookieParser from "cookie-parser";
 
@@ -17,6 +19,7 @@ app.use(cookieParser());
 //Using Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 //Basic Route to check if the server is working
 app.get("/", (req, res) => {
