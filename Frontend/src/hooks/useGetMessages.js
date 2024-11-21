83b8a0a7684053ed4ce7d105606e64c53db2c4cf
Shail <shailjain2709@gmail.com>
2024-11-21 12:57:51 +0530

    Added Get Message Functionality

---
+++ Frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import useConversation from "../zustand/useConversation";
+import toast from 'react-hot-toast';
+
+const useGetMessages = () => {
+    const [loading, setLoading] = useState(false);
+    const { messages, setMessages, selectedConversation } = useConversation();
+    useEffect(() => {
+        const getMessages = async () => {
+            try {
+                setLoading(true);
+                const res = await axios.get(`http://localhost:4000/api/messages/${selectedConversation._id}`, { withCredentials: true })
+                const data = res.data;
+                if (res.data.error) {
+                    throw new Error(res.data.error);
+                }
+                setMessages(data);
+            } catch (error) {
+                toast.error(error.message);
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        if (selectedConversation?._id) getMessages();
+    }, [selectedConversation?._id, setMessages])
+    return { messages, loading };
+}
+
+export default useGetMessages;
