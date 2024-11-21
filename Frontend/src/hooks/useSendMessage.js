5a8fab633117c23e7f586674a6ef420f2688d7e6
Shail <shailjain2709@gmail.com>
2024-11-21 12:02:50 +0530

    Added Send Message Functonality

---
+++ Frontend/src/hooks/useSendMessage.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useState } from "react"
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
+
+const useSendMessage = () => {
+    const [loading, setLoading] = useState(false)
+    const { messages, setMessages, selectedConversation } = useConversation();
+
+    const sendMessage = async (message) => {
+        setLoading(true);
+        try {
+            const res = await axios.post(`http://localhost:4000/api/messages/send/${selectedConversation._id}`, { message }, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                withCredentials: true,  // Adding withCredentials
+            });
+            const data = res.data;
+            if (data.error) throw new Error(data.error);
+
+            setMessages([...messages, data]);
+        } catch (error) {
+            toast.error(error.message)
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+    return { sendMessage, loading }
+}
+
+export default useSendMessage
